91057295ce5595a4a7476e57fd3e387c4fd6e687
Uthaeus <Uth@github.com>
2018-02-02 16:33:25 -0700

    Implemented cards for documents index page

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {
       description: 'adsfasdlkfjas;fj adkfja;f',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'https://collectivehub.com/wp-content/uploads/2016/07/Things_successful_freelancers_do_at_home_body-1.jpg'
     },
     {
       title: "My Second Doc",
       description: 'adsfasdlkfjas;fj adkfja;f',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'https://assets.entrepreneur.com/content/3x2/1300/20180111134829-freelance-2906725-1920.png?width=750&crop=16:9'
     },
     {
       title: "My Last Doc",
       description: 'adsfasdlkfjas;fj adkfja;f',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'https://tctechcrunch2011.files.wordpress.com/2015/12/shutterstock_279072494.png'
     }
   ]
 }
