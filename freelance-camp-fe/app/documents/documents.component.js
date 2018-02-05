6175224f34ecd00f71844b431e15fb025e58ac36
Uthaeus <Uth@github.com>
2018-02-05 14:52:07 -0700

    Created api connection for document data

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -5,42 +5,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var Rx_1 = require("rxjs/Rx");
+var document_service_1 = require("./document.service");
 var DocumentsComponent = /** @class */ (function () {
-    function DocumentsComponent() {
+    function DocumentsComponent(documentService) {
+        this.documentService = documentService;
         this.pageTitle = "Document Dashboard";
-        this.documents = [
-            {
-                title: "My first Doc",
-                description: 'adsfasdlkfjas;fj adkfja;f',
-                file_url: 'http://google.com',
-                updated_at: '11/11/16',
-                image_url: 'https://collectivehub.com/wp-content/uploads/2016/07/Things_successful_freelancers_do_at_home_body-1.jpg'
-            },
-            {
-                title: "My Second Doc",
-                description: 'adsfasdlkfjas;fj adkfja;f',
-                file_url: 'http://google.com',
-                updated_at: '11/11/16',
-                image_url: 'https://assets.entrepreneur.com/content/3x2/1300/20180111134829-freelance-2906725-1920.png?width=750&crop=16:9'
-            },
-            {
-                title: "My Last Doc",
-                description: 'adsfasdlkfjas;fj adkfja;f',
-                file_url: 'http://google.com',
-                updated_at: '11/11/16',
-                image_url: 'https://tctechcrunch2011.files.wordpress.com/2015/12/shutterstock_279072494.png'
-            }
-        ];
+        this.mode = "Observable";
     }
+    DocumentsComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        var timer = Rx_1.Observable.timer(0, 5000);
+        timer.subscribe(function () { return _this.getDocuments(); });
+    };
+    DocumentsComponent.prototype.getDocuments = function () {
+        var _this = this;
+        this.documentService.getDocuments()
+            .subscribe(function (documents) { return _this.documents = documents; }, function (error) { return _this.errorMessage = error; });
+    };
     DocumentsComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'documents',
             templateUrl: 'documents.component.html',
-            styleUrls: ['documents.component.css']
-        })
+            styleUrls: ['documents.component.css'],
+            providers: [document_service_1.DocumentService]
+        }),
+        __metadata("design:paramtypes", [document_service_1.DocumentService])
     ], DocumentsComponent);
     return DocumentsComponent;
 }());
--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,35 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
-  documents: Document[] = [
-    {
-      title: "My first Doc",
-      description: 'adsfasdlkfjas;fj adkfja;f',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://collectivehub.com/wp-content/uploads/2016/07/Things_successful_freelancers_do_at_home_body-1.jpg'
-    },
-    {
-      title: "My Second Doc",
-      description: 'adsfasdlkfjas;fj adkfja;f',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://assets.entrepreneur.com/content/3x2/1300/20180111134829-freelance-2906725-1920.png?width=750&crop=16:9'
-    },
-    {
-      title: "My Last Doc",
-      description: 'adsfasdlkfjas;fj adkfja;f',
-      file_url: 'http://google.com',
-      updated_at: '11/11/16',
-      image_url: 'https://tctechcrunch2011.files.wordpress.com/2015/12/shutterstock_279072494.png'
-    }
-  ]
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private documentService: DocumentService
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+            documents => this.documents = documents,
+            error => this.errorMessage = <any>error 
+        );
+  }
 }
