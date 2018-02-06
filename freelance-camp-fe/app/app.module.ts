16751ab380b3a840f07997f2668c0cbbae5f567b
Uthaeus <Uth@github.com>
2018-02-06 11:46:59 -0700

    Final changes to app

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -32,7 +32,8 @@ import { AppRoutingModule } from './app-routing.module';
     ProposalShowComponent
   ],
   providers: [
-    DocumentService
+    DocumentService,
+    ProposalService
   ],
   bootstrap: [
     AppComponent
